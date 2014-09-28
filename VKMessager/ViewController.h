8b593442bb29c84190f79b267dff85c8e07e8d11
SnowyWhite <nick.kibish@gmail.com>
2014-09-28 12:00:30 +0300

    Initial Commit

---
+++ VKMessager/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  VKMessager
+//
+//  Created by Nick Kibish on 28.09.14.
+//  Copyright (c) 2014 Nick Kibish. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
